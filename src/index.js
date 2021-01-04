8c13639879b624b6ba1b7455db4f6b4c211a99b4
Evyatar Yagoni <evyataryagoni@gmail.com>
2021-01-05 01:21:50 +0200

    finish version 1.0.0

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-//
+
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
